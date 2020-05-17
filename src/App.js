03ed87a624deb6b275072f8a9836a2ab18560607
mva66 <mva6644@gmail.com>
2020-05-17 14:43:29 -0400

    Task 4.4

--- src/App.js
+++ src/App.js
@@ -27,27 +27,3 @@ class App extends Component {
 }
 
 export default App;
-
-// import React, { Component } from "react";
-// import "./App.css";
-// import EventList from "./EventList";
-// import CitySearch from "./CitySearch";
-// import NumberOfEvents from "./NumberOfEvents";
-// import { getEvents } from "./api";
-
-// class App extends Component {
-//   updateEvents = (lat, lon) => {
-//     getEvents(lat, lon).then((events) => this.setState({ events }));
-//   };
-//   render() {
-//     return (
-//       <div className="App">
-//         <CitySearch updateEvents={this.updateEvents} />
-//         <EventList />
-//         <NumberOfEvents />
-//       </div>
-//     );
-//   }
-// }
-
-// export default App;
--- src/EventList.js
+++ src/EventList.js
@@ -16,28 +16,3 @@ class EventList extends Component {
 }
 
 export default EventList;
-
-// import React, { Component } from "react";
-// import Event from "./Event";
-
-// class EventList extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       events: [],
-//     };
-//   }
-//   render() {
-//     return (
-//       <ul className="EventList">
-//         {this.state.events.map((event) => (
-//           <li key={event.id}>
-//             <Event event={event} />
-//           </li>
-//         ))}
-//       </ul>
-//     );
-//   }
-// }
-
-// export default EventList;
