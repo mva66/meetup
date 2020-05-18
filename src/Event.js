bbbf9dd71509d4800edd7e283e7e1079988cc008
mva66 <mva6644@gmail.com>
2020-05-17 23:12:28 -0400

    Task 4.4 changes

--- src/Event.js
+++ src/Event.js
@@ -2,15 +2,12 @@ import React, { Component } from "react";
 
 class Event extends Component {
   state = {
+    event: {},
     showDetails: false,
   };
 
   handleShowDetails = () => {
-    if (this.state.showDetails === false) {
-      this.setState({ showDetails: true });
-    } else {
-      this.setState({ showDetails: false });
-    }
+    this.setState({ showDetails: true });
   };
 
   render() {
@@ -21,19 +18,14 @@ class Event extends Component {
         <div className="event__Overview">
           <p className="event__Overview--name">{this.props.event.name}</p>
           <p className="event__Overview--localDate">
-            {this.props.event.local_date}
+            {this.state.event.local_date}
           </p>
-          <button
-            className="details-btn"
-            onClick={() => this.handleShowDetails()}
-          >
-            show details
-          </button>
+          <button onClick={() => this.handleShowDetails()}>show details</button>
         </div>
         {showDetails && (
           <div className="event__Details">
             <p className="event__Details--description">
-              {this.props.event.description}
+              {this.state.event.description}
             </p>
           </div>
         )}
@@ -43,41 +35,3 @@ class Event extends Component {
 }
 
 export default Event;
-
-// import React, { Component } from "react";
-
-// class Event extends Component {
-//   state = {
-//     event: {},
-//     showDetails: false,
-//   };
-
-//   handleShowDetails = () => {
-//     this.setState({ showDetails: true });
-//   };
-
-//   render() {
-//     const showDetails = this.state.showDetails;
-
-//     return (
-//       <div className="event">
-//         <div className="event__Overview">
-//           <p className="event__Overview--name">{this.state.event.name}</p>
-//           <p className="event__Overview--localDate">
-//             {this.state.event.local_date}
-//           </p>
-//           <button onClick={() => this.handleShowDetails()}>show details</button>
-//         </div>
-//         {showDetails && (
-//           <div className="event__Details">
-//             <p className="event__Details--description">
-//               {this.state.event.description}
-//             </p>
-//           </div>
-//         )}
-//       </div>
-//     );
-//   }
-// }
-
-// export default Event;
