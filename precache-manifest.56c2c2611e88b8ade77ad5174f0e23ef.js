6417f35c25c2a2495bfb8b4f1262269886c3da1e
mva66 <mva6644@gmail.com>
2020-05-18 13:15:18 -0400

    Updates

--- precache-manifest.56c2c2611e88b8ade77ad5174f0e23ef.js
+++ precache-manifest.56c2c2611e88b8ade77ad5174f0e23ef.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "cc80e845bb137159ac2a51fd06994127",
+    "revision": "276e9bfe83d372523d700cb8c72a8320",
     "url": "/meetup/index.html"
   },
   {
-    "revision": "486f453759d40197aa84",
+    "revision": "8b98503abbaffcb4bc74",
     "url": "/meetup/static/css/main.60c4dbda.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/meetup/static/js/2.d72b632a.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "486f453759d40197aa84",
-    "url": "/meetup/static/js/main.b8e75e3f.chunk.js"
+    "revision": "8b98503abbaffcb4bc74",
+    "url": "/meetup/static/js/main.8720b913.chunk.js"
   },
   {
     "revision": "dc40033a0623ebf9058f",
