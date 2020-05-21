e54034b03a14c9a17ea83a59dac693d1fa254ff6
mva66 <mva6644@gmail.com>
2020-05-20 20:01:23 -0400

    Updates

--- precache-manifest.d0d88a616f2fb49d640ac90c3c05e949.js
+++ precache-manifest.d0d88a616f2fb49d640ac90c3c05e949.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "1cd110a1201f5bb0932a85b323c33b30",
+    "revision": "67d9a2b00a8661ab30b6535edbacecab",
     "url": "/meetup/index.html"
   },
   {
-    "revision": "99c7e82027efa6a14156",
+    "revision": "3856e3e7ec32e4f543e4",
     "url": "/meetup/static/css/main.60c4dbda.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/meetup/static/js/2.f33e5431.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "99c7e82027efa6a14156",
-    "url": "/meetup/static/js/main.fe671354.chunk.js"
+    "revision": "3856e3e7ec32e4f543e4",
+    "url": "/meetup/static/js/main.abc18bb8.chunk.js"
   },
   {
     "revision": "dc40033a0623ebf9058f",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/meetup/precache-manifest.33fc9c0006026761e6d8141d9a93ba1c.js"
+  "/meetup/precache-manifest.d0d88a616f2fb49d640ac90c3c05e949.js"
 );
 
 self.addEventListener('message', (event) => {
