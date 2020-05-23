c1c7fcb24dea336aceb44e9fb914dbcd0f76171e
mva66 <mva6644@gmail.com>
2020-05-23 10:59:27 -0400

    Task 4.9 changes

--- src/Event.js
+++ src/Event.js
@@ -1,8 +1,18 @@
 import React, { Component } from "react";
+import {
+  PieChart,
+  Pie,
+  Legend,
+  Tooltip,
+  ResponsiveContainer,
+  Line,
+  Cell,
+} from "recharts";
 
 class Event extends Component {
   state = {
     showDetails: false,
+    events: [],
   };
 
   handleShowDetails = () => {
@@ -13,7 +23,19 @@ class Event extends Component {
     }
   };
 
+  getData = () => {
+    const taken = this.props.event.yes_rsvp_count;
+    const limit = this.props.event.rsvp_limit;
+    const open = limit - taken;
+
+    return [
+      { name: "Reservations", value: taken },
+      { name: "Free slots", value: open },
+    ];
+  };
   render() {
+    const { event } = this.props;
+    let colors = ["#8884d8", "#82ca9d"];
     const showDetails = this.state.showDetails;
 
     return (
@@ -31,7 +53,44 @@ class Event extends Component {
           </button>
         </div>
         {showDetails && (
-          <div className="event__Details">
+          <div className="eventDetails">
+            {event.yes_rsvp_count && event.rsvp_limit ? (
+              <ResponsiveContainer height={200}>
+                <PieChart width={200} height={200}>
+                  <Pie
+                    data={this.getData()}
+                    dataKey="value"
+                    nameKey="name"
+                    cx="50%"
+                    cy="50%"
+                    outerRadius={80}
+                    fill="#8884d8"
+                    label
+                  >
+                    {this.getData().map((entry, index) => (
+                      <Cell key={`cell-${index}`} fill={colors[index]} />
+                    ))}
+                  </Pie>
+
+                  <Tooltip />
+
+                  <Legend verticalAlign="top" height={36}>
+                    <Line
+                      name="Reservations"
+                      type="monotone"
+                      dataKey="taken"
+                      stroke="#8884d8"
+                    />
+                    <Line
+                      name="Free Slots"
+                      type="monotone"
+                      dataKey="open"
+                      stroke="#82ca9d"
+                    />
+                  </Legend>
+                </PieChart>
+              </ResponsiveContainer>
+            ) : null}
             <div className="eventVenue">
               Venue:{" "}
               {this.props.event.venue && this.props.event.venue.address_1}
